1e8a29ccdc539f1607c1f7b9aee0dfb67117d5b4
nicolas-lorenzo <nicolas.lorenzo@student.kdg.be>
2026-01-07 22:10:53 +0100

    improving naming

--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -2,20 +2,20 @@ from pathlib import Path
 
 from models.extracted_data.customer_orders_extracted_data import CustomerOrdersExtractedData
 from sqlalchemy import Engine
-from pipeline.extractors.customer_orders.db_tables_extractor import load_customers, load_orders, \
-    load_currency_exchanges
-from pipeline.extractors.customer_orders.order_rows_extractor import load_order_rows_from_parquet
-from pipeline.extractors.customer_orders.products_extractor import load_products_from_json
-from pipeline.extractors.customer_orders.stores_extractor import load_stores_from_csv
+from pipeline.extractors.customer_orders.db_tables_extractor import extract_customers, extract_orders, \
+    extract_currency_exchanges
+from pipeline.extractors.customer_orders.order_rows_extractor import extract_order_rows_from_parquet
+from pipeline.extractors.customer_orders.products_extractor import extract_products_from_json
+from pipeline.extractors.customer_orders.stores_extractor import extract_stores_from_csv
 
 
 def extract_data(engine: Engine, stores_path:Path, products_path: Path, order_rows_path: Path) -> CustomerOrdersExtractedData:
-    customer_list = load_customers(engine)
-    order_list = load_orders(engine)
-    currency_exchange_list = load_currency_exchanges(engine)
-    store_list = load_stores_from_csv(stores_path)
-    products_list = load_products_from_json(products_path)
-    order_rows_list = load_order_rows_from_parquet(order_rows_path)
+    customer_list = extract_customers(engine)
+    order_list = extract_orders(engine)
+    currency_exchange_list = extract_currency_exchanges(engine)
+    store_list = extract_stores_from_csv(stores_path)
+    products_list = extract_products_from_json(products_path)
+    order_rows_list = extract_order_rows_from_parquet(order_rows_path)
 
     customer_orders_all_extracted_data = CustomerOrdersExtractedData(customer_list, order_list, currency_exchange_list,
                                                                      store_list, products_list, order_rows_list)
--- src/pipeline/extractors/customer_orders/products_extractor.py
+++ src/pipeline/extractors/customer_orders/products_extractor.py
@@ -4,7 +4,7 @@ from models.customer_orders.product import Product
 from pipeline.utils.file_readers import ensure_file_exists, read_json_or_fail
 
 
-def load_products_from_json(products_json_path: Path) -> list[Product]:
+def extract_products_from_json(products_json_path: Path) -> list[Product]:
     ensure_file_exists(products_json_path)
     products: list[dict[str, str]] = read_json_or_fail(products_json_path)
     return [Product.model_validate(product) for product in products]
