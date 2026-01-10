9648a504b2111350959a8bab18bb27becb5b3b0b
nicolas-lorenzo <nicolas.lorenzo@student.kdg.be>
2026-01-10 12:56:52 +0100

    I am able to load stores

--- src/pipeline/utils/strings.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-
-def strip_string(value: Optional[str]) -> Optional[str]:
-    if value is None:
-        return None
-    value = str(value).strip()
-    return value or None
